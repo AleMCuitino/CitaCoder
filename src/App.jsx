9c5db56fc1970413831bb8e43c1a34853b59292a
AleMCuitino <ale.morales11@gmail.com>
2022-12-28 11:24:15 +0100

    install libreries

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="Title">Cita coder</h1>
+    </div>
+  );
+}
+
+export default App;
